efca8ffdccd95b529cbb72e74e09ce8af59aff7b
Johan <johan.garcia165@gmail.com>
2024-05-12 13:08:55 -0400

    Bajando back

--- src/database/connections.js
+++ src/database/connections.js
@@ -1,23 +1,22 @@
-import sql from 'mssql'
+import sql from "mssql";
 
 const dbSettings = {
-    user: "SA", ////este usuario es por defecto, si no te agarra a ti seguro es en minusculas
-    password:"Orion.2023*/", /// clave del servidor 
-    server:"localhost",
-    database: "DB_RODAMITRE_WEB",////nombre de la base de datos
-    options: {
-        encrypt:true,
-        trustServerCertificate: true
-    }
-}
-
+  user: "sa", ////este usuario es por defecto, si no te agarra a ti seguro es en minusculas
+  password: "24656399", /// clave del servidor
+  server: "localhost",
+  database: "RODAMITRE_WEB_ATIKA_NUEVA", ////nombre de la base de datos
+  requestTimeout: 300000,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
 
 export const getConnection = async () => {
-    try { 
-const pool = await sql.connect(dbSettings)
+  try {
+    const pool = await sql.connect(dbSettings);
     return pool;
-    } catch (error){
-        console.log(error)
-    }   
-}
-
+  } catch (error) {
+    console.log(error);
+  }
+};
